1c5c6af14877b558ecfddbe858cf5ea786d3805d
FaeronGaming <animeniac@live.com>
2022-07-28 16:26:58 -0600

    Update provider for planetscale

--- prisma/migrations/20220722182038_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateTable
-CREATE TABLE "Webhook" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "url" TEXT NOT NULL
-);
--- prisma/migrations/20220722183826_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[url]` on the table `Webhook` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Webhook_url_key" ON "Webhook"("url");
